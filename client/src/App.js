4f50d8894ab9eec041fbda545c5bc7d8399d3675
utsav28 <utsavkhatu416@gmail.com>
2023-02-02 23:48:25 +0530

    Add Product Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import UserProducts from './pages/user/UserProducts';
 import UserOffers from './pages/user/UserOffers';
 import UserSettings from './pages/user/UserSettings';
 import Products from './pages/product/Products2';
+import AddProduct from './pages/product/AddProduct';
 
 function App() {
     return (
@@ -19,6 +20,7 @@ function App() {
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/user" element={<UserProfile />} />
                 <Route path="/user/product" element={<UserProducts />} />
+                <Route path="/user/product/add" element={<AddProduct />} />
                 <Route path="/user/offer" element={<UserOffers />} />
                 <Route path="/user/setting" element={<UserSettings />} />
                 <Route path='/products' element={<Products />} />
