4cb584a5705c9cc92d5271a4eb318adadcb3e87d
utsav28 <utsavkhatu416@gmail.com>
2023-02-03 06:49:08 +0530

    Minor Fixes

--- client/src/pages/product/OfferProducts.js
+++ client/src/pages/product/OfferProducts.js
@@ -155,66 +155,69 @@ export default function OfferProducts() {
                         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                             {trades.map((product) => {
                                 return (
-                                    <div key={product.id}>
-                                        <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                                            <img
-                                                src={product.sell.images[0]}
-                                                alt={product.imageAlt}
-                                                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                            />
-                                        </div>
-                                        <div className="mt-4 flex justify-between">
-                                            <div>
-                                                <h3 className="text-sm text-gray-700">
-                                                    {/* <a href={product.href}> */}
-                                                    <span
-                                                        aria-hidden="true"
-                                                        className="absolute inset-0"
-                                                    />
-                                                    {product.sell.name}
-                                                    {/* </a> */}
-                                                </h3>
-                                                <p className="mt-1 text-sm text-gray-500">
-                                                    {product.sell.description}
+                                    <>
+                                        <div key={product.id}>
+                                            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                                                <img
+                                                    src={product.sell.images[0]}
+                                                    alt={product.imageAlt}
+                                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                                />
+                                            </div>
+                                            <div className="mt-4 flex justify-between">
+                                                <div>
+                                                    <h3 className="text-sm text-gray-700">
+                                                        {/* <a href={product.href}> */}
+                                                        <span
+                                                            aria-hidden="true"
+                                                            className="absolute inset-0"
+                                                        />
+                                                        {product.sell.name}
+                                                        {/* </a> */}
+                                                    </h3>
+                                                    <p className="mt-1 text-sm text-gray-500">
+                                                        {product.sell.description}
+                                                    </p>
+                                                </div>
+                                                <p className="text-sm font-medium text-gray-900">
+                                                    {product.sell.price}
                                                 </p>
                                             </div>
-                                            <p className="text-sm font-medium text-gray-900">
-                                                {product.sell.price}
-                                            </p>
                                         </div>
-                                    </div>
+                                        <div className="flex flex-wrap justify-center">
+                                            <button
+                                                type="button"
+                                                className="py-2 px-4  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg m-2"
+                                                onClick={() => {
+                                                    console.log("hello")
+                                                }}
+                                            >
+                                                <CloseIcon />
+                                            </button>
+                                            <button
+                                                type="button"
+                                                className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg m-2"
+                                                onClick={() => {
+                                                    console.log("hello")
+                                                }}
+                                            >
+                                                <ChatIcon />
+                                            </button>
+                                            <button
+                                                type="button"
+                                                className="py-2 px-4  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg m-2"
+                                                onClick={() => {
+                                                    console.log("hello")
+                                                }}
+                                            >
+                                                <DoneIcon />
+                                            </button>
+                                        </div>
+                                    </>
                                 );
                             })}
                         </div>
-                        <div className="flex flex-wrap justify-center">
-                            <button
-                                type="button"
-                                className="py-2 px-4  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg m-2"
-                                onClick={() => {
-                                    console.log("hello")
-                                }}
-                            >
-                                <CloseIcon />
-                            </button>
-                            <button
-                                type="button"
-                                className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg m-2"
-                                onClick={() => {
-                                    console.log("hello")
-                                }}
-                            >
-                                <ChatIcon />
-                            </button>
-                            <button
-                                type="button"
-                                className="py-2 px-4  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg m-2"
-                                onClick={() => {
-                                    console.log("hello")
-                                }}
-                            >
-                                <DoneIcon />
-                            </button>
-                        </div>
+
                     </div>
                 </div>
             </div>
