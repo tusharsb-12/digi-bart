548ba51297cdb22e37250a99a4d04fcd8e3f4c9b
Tushar <btushar1208@gmail.com>
2023-02-03 05:53:07 +0530

    .

--- server/src/api/trade.ts
+++ server/src/api/trade.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authenticate from '../middleware/auth';
 import {
+    acceptTrade,
     barterOffer,
     createTrade,
+    rejectTrade,
     userOffers,
     userTrades,
 } from '../controllers/trade';
@@ -12,6 +14,8 @@ const router = Router();
 router.post('/create-trade', createTrade);
 router.get('/my-trades', authenticate, userTrades);
 router.get('/my-offers', authenticate, userOffers);
-router.post('/barter-offer/:id', barterOffer);
+router.get('/barter-offer/:id', barterOffer);
+router.post('/accept-trade/:id', acceptTrade);
+router.post('/reject-trade/:id', rejectTrade);
 
 export default router;
